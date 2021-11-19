767dc8e5aeccda66e9e2ec14baa1e6c9a6880268
Manu Mathew <manu.2.mathew@nokia.com>
2021-11-19 14:38:33 +0530

    Update the UI graphql client code

--- angular-ui/src/app/app.component.ts
+++ angular-ui/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { Student } from './app.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-ui';
+  students: Array<Student> = [];
+  departments = [];
+  loading = false;
+  error = '';
+
+  constructor(private apollo: Apollo) { }
+
+  ngOnInit() {
+    console.log("In...1");
+    this.apollo
+      .watchQuery({
+        query: gql`
+        {
+          students {
+            student_id,
+            student_name,
+            student_age
+          }
+        }
+        `,
+      })
+      .valueChanges.subscribe((result: any) => {
+        console.log("In...2");
+        const responseData = result.data;
+        this.loading = result.loading;
+        this.error = result.error;
+
+        this.students = responseData.students;
+        //this.departments = responseData.departments;
+      });
+  }
 }
---
+++ angular-ui/src/app/app.model.ts
@@ -0,0 +1,5 @@
+export interface Student {
+    student_id: string,
+    student_name: string;
+    student_age: number;
+}
